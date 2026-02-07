0887906edd458306f00a6098d4dfff38acebe883
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-07 11:11:28 +0200

    Update console.php
    Répondre

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
-
+// je sais pas leurs vraie travail mais bon en vue de la compréhension seulement 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
