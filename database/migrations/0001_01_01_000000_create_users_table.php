22344bf724b8ef2425374cb3c0a2dd896f1bf855
Reddy-Nzoloko <reddynzoloko@gmail.com>
2026-02-08 10:36:53 +0200

    Update 0001_01_01_000000_create_users_table.php
    Mise en jour

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration
 {
     /**
+    les mises a jours journaliered 
      * Run the migrations.
      */
     public function up(): void
